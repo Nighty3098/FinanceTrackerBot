81cec8959d422b3216043727f50f9e7a609bc169
night3098 <134074263+night3098@users.noreply.github.com>
2024-02-27 15:59:13 +0700

    Add new categories

--- categories.py
+++ categories.py
@@ -2,7 +2,7 @@ from config import *
 
 categories = {
     "Drive": ["такси", "поездка", "автобус", "метро", "машина", "поезд"],
-    "Food": ["еда", "ресторан", "похавать", "кофе"],
+    "Food": ["еда", "ресторан", "похавать", "кофе", "кафе"],
     "Devices": ["днс", "девайсы", "пк"],
     "Subscription": [
         "подписка",
@@ -15,11 +15,14 @@ categories = {
     ],
     "Books": ["книги"],
     "Other": ["другое", " "],
+    "Taxes": ["налоги", "жэу", "жкх", "налог"],
+    "Courses": ["курсы", "обучение", "учёба"],
     "Work": ["фриланс", "работа", "программирование", "разработка"],
 }
 
 
 async def change_category(text):
+    text = text.lower()
     for category, words in categories.items():
         if any(word in text for word in words):
             return category
